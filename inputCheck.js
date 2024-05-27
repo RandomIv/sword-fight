12df6210bbab19029c2a269f75e6428c33329ce4
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-05-27 22:29:40 +0300

    Changed from 'event.key' to 'event.code' to check input

--- inputCheck.js
+++ inputCheck.js
@@ -36,30 +36,30 @@ export const keys = {
 };
 
 document.addEventListener('keydown', (event) => {
-  if (event.key === 'r') {
+  if (event.code === 'KeyR') {
     keys.r.isPressed = true;
   }
 
   //for player1
   if (!player1.isAttacking) {
-    switch (event.key) {
-      case 'a':
+    switch (event.code) {
+      case 'KeyA':
         keys.a.isPressed = true;
         player1.lastKey = 'a';
         break;
-      case 'd':
+      case 'KeyD':
         keys.d.isPressed = true;
         player1.lastKey = 'd';
         break;
-      case 'w':
+      case 'KeyW':
         keys.w.isPressed = true;
         player1.stance = 'High';
         break;
-      case 's':
+      case 'KeyS':
         keys.s.isPressed = true;
         player1.stance = 'Low';
         break;
-      case ' ':
+      case 'Space':
         player1.attack();
         break;
     }
@@ -67,7 +67,7 @@ document.addEventListener('keydown', (event) => {
 
   //for player2
   if (!player2.isAttacking) {
-    switch (event.key) {
+    switch (event.code) {
       case 'ArrowLeft':
         keys.arrowLeft.isPressed = true;
         player2.lastKey = 'arrowLeft';
@@ -84,7 +84,7 @@ document.addEventListener('keydown', (event) => {
         keys.arrowDown.isPressed = true;
         player2.stance = 'Low';
         break;
-      case 'z':
+      case 'KeyZ':
         player2.attack();
         break;
     }
@@ -92,24 +92,24 @@ document.addEventListener('keydown', (event) => {
 });
 
 document.addEventListener('keyup', (event) => {
-  if (event.key === 'r') {
+  if (event.code === 'KeyR') {
     keys.r.isPressed = false;
   }
 
   //for player1
   if (!player1.isAttacking) {
-    switch (event.key) {
-      case 'a':
+    switch (event.code) {
+      case 'KeyA':
         keys.a.isPressed = false;
         break;
-      case 'd':
+      case 'KeyD':
         keys.d.isPressed = false;
         break;
-      case 'w':
+      case 'KeyW':
         keys.w.isPressed = false;
         player1.stance = 'Middle';
         break;
-      case 's':
+      case 'KeyS':
         keys.s.isPressed = false;
         player1.stance = 'Middle';
         break;
@@ -118,7 +118,7 @@ document.addEventListener('keyup', (event) => {
 
   //for player2
   if (!player2.isAttacking) {
-    switch (event.key) {
+    switch (event.code) {
       case 'ArrowLeft':
         keys.arrowLeft.isPressed = false;
         break;
